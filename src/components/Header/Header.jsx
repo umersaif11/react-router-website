a1216af4c5038993799345bd2dece3c1f5d3db31
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-03 17:37:38 +0500

    Use the Link and NavLink inplace to a tag so that page dont get refreshed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,10 @@ export default function Header() {
                             <li>
                                 <NavLink
                                     className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
+                                        
+                                        hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
+                                        hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
