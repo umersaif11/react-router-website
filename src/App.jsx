97d9871541f5cb602e2eaf156871646d27c04e8f
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-03 18:49:35 +0500

    Create home component in components/Home folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-600 p-4'>
-        React Router
-      </h1>
+      <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
