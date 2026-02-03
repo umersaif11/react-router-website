3864719298470a801bc161dcbcd18bd5aa7d32d6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-03 17:19:18 +0500

    Uninstall react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-600 p-4 '>
+      <h1 className='bg-green-600 p-4'>
         React Router
       </h1>
     </>
