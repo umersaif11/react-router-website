7e378448875c8ebcb5be3a13d69032eff5ce78d6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-03 21:41:36 +0500

    A new illustration for the about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import aboutImg from '../../assets/about.png'
 
 export default function About() {
     return (
@@ -7,7 +8,7 @@ export default function About() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src={aboutImg}
                             alt="image"
                         />
                     </div>
