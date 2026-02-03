12b139c78fb539d9753911a51f558747496b66af
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-03 21:28:41 +0500

    Add images for the home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import remoteImage1 from '../../assets/remote1.png'
+import remoteImage2 from '../../assets/remote2.png'
 
 export default function Home() {
     return (
@@ -32,12 +34,12 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img className="w-96" src={remoteImage1} alt="image1" />
                 </div>
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img className="sm:w-96 w-48" src={remoteImage2} alt="image2" />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
