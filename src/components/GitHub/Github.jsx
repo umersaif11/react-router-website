25eea63ca43a780b1317538bc702e60be816c83e
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-04 15:34:35 +0500

    Fetch the data from github api and display it in the github component

--- src/components/GitHub/Github.jsx
+++ src/components/GitHub/Github.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+    const [mydata, setmydata] = useState({})
+    useEffect(() => {
+        fetch('https://api.github.com/users/umersaif11')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setmydata(data)
+        })
+    }, [])
   return (
-    <div className='text-3xl text-white bg-gray-600 p-4 m-4 text-center'>
-        Github
+    <div 
+    className='text-3xl text-white bg-gray-600 p-4 m-4 text-center flex gap-4'>
+        <img src={mydata.avatar_url} alt="" />
+        <div className='flex flex-col gap-6 justify-around'>
+            <div>Github Name: <span className='text-orange-600'>{mydata.name}</span></div>
+            <div className='text-start'>Github Followers: <span className='text-green-600'>{mydata.followers}</span></div>
+            <div className='text-start'>Github Following: <span className='text-cyan-600'>{mydata.following}</span></div>
+        </div>
     </div>
   )
 }
