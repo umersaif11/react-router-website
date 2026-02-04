1af45c7906043b4ea1873de8de2df2ccb41af1c0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-04 15:02:47 +0500

    Add another component's(github) routing

---
+++ src/components/GitHub/Github.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Github() {
+  return (
+    <div className='text-3xl text-white bg-gray-600 p-4 m-4 text-center'>
+        Github
+    </div>
+  )
+}
+
+export default Github
