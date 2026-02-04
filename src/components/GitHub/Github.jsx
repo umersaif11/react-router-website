322e461e1cf6bf13a32f004ffcedd0821f178f1b
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-04 17:54:51 +0500

    Use the loader attribute in the Route component in main.jsx and make the fetch api call in Github.jsx a separate method, and use that as the loader value, userLoaderData hook automatically take the result of the fetch api call - all of this headache is just because loader attribute allow us to prefetch meaning as soon as 1) react load or 2) you hover over github icon, then the fetch will start its function and save the result in memory

--- src/components/GitHub/Github.jsx
+++ src/components/GitHub/Github.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [mydata, setmydata] = useState({})
-    useEffect(() => {
-        fetch('https://api.github.com/users/umersaif11')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            setmydata(data)
-        })
-    }, [])
+    // const [mydata, setmydata] = useState({})
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/umersaif11')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setmydata(data)
+    //     })
+    // }, [])
+    const mydata = useLoaderData()
   return (
     <div 
     className='text-3xl text-white bg-gray-600 p-4 m-4 text-center flex gap-4'>
@@ -24,3 +26,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/umersaif11')
+    return response.json()
+}
