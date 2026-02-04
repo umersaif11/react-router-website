fd0ab67b68201c9ee51031379eb45d6925cad508
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-04 14:38:08 +0500

    Take the variable userid originally defined after the : in the path in main.jsx

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function User() {
+    const { userid } = useParams()
   return (
-    <div>User</div>
+    <div className='bg-gray-600 text-3xl text-white p-4'>
+        User: {userid}
+    </div>
   )
 }
 
